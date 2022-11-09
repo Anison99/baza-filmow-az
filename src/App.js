215cc4a8380fb7ee916765293472020e41463ef3
anison99_ <anita.zych9941@gmail.com>
2022-11-09 13:20:50 +0100

    add movie posters and refactor app interface

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
 import './Banner.css';
+import Movie from './Movie';
 import Navbar from './Navbar';
 import Banner from './Banner';
+import Shrek1 from './movie-images/shrek-1.jpg'
+import Shining from './movie-images/shining.png'
+import Alien from './movie-images/obcy.png'
+import Stranger from './movie-images/stranger-things-3.jpg'
 import "bootstrap/dist/css/bootstrap.min.css";
 const App = () => {
     
@@ -11,6 +16,76 @@ const App = () => {
         <div className="body">
             <Navbar /> 
             <Banner />
+            <div>
+                <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item border-0">
+                        <div className="movie">
+                            <img src={Shrek1}></img>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                                <button class=" btn btn-success" type="button" >ZOBACZ</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item border-0">
+                        <div className="movie">
+                            <img src={Shining}></img>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                                <button class=" btn btn-success" type="button">ZOBACZ</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item border-0">
+                        <div className="movie">
+                            <img src={Alien}></img>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                                <button class=" btn btn-success" type="button">ZOBACZ</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item border-0">
+                        <div className="movie">
+                            <img src={Stranger}></img>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                                <button class=" btn btn-success" type="button">ZOBACZ</button>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="list-group list-group-horizontal-sm">
+                    <li class="list-group-item border-0">
+                        <div className="movie">
+                            <img src={Shrek1}></img>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                                <button class=" btn btn-success" type="button" >ZOBACZ</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item border-0">
+                        <div className="movie">
+                            <img src={Shining}></img>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                                <button class=" btn btn-success" type="button">ZOBACZ</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item border-0">
+                        <div className="movie">
+                            <img src={Alien}></img>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                                <button class=" btn btn-success" type="button">ZOBACZ</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item border-0">
+                        <div className="movie">
+                            <img src={Stranger}></img>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                                <button class=" btn btn-success" type="button">ZOBACZ</button>
+                            </div>
+                        </div>
+                    </li>
+                </ul> 
+             </div>
         </div>   
     );
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Movie.css';
+import Shrek1 from './movie-images/shrek-1.jpg'
+import Shining from './movie-images/shining.png'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
 const Movie = () => {
     return (
-        <header>
-            
-        </header>
+        <>
+        </>
+     
     );
 }
 
