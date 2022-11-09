f8a8b503f9b2f8cee38ddcf95150307c9765188c
anison99_ <anita.zych9941@gmail.com>
2022-11-09 11:01:14 +0100

    add Movie file

---
+++ src/Movie.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Movie.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const Movie = () => {
+    return (
+        <header>
+            
+        </header>
+    );
+}
+
+export default Movie;
